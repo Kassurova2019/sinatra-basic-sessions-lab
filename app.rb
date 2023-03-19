a5485d0fc7e75858b805a76834f588f6c5a573f8
Luba Kassurova <kassurova@icloud.com>
2023-03-19 16:52:01 -0400

    commit tree 1e139a182dd5918bf85f48ec25bf6593531d7eff

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
-
+require "pry"
 class App < Sinatra::Base
+
+    # configure do
+    #     enable :sessions unless test?
+    #     set :session_secret, "secret"
+    # end
+
+    get '/' do 
+        erb :index
+    end
+    post '/checkout' do
+        session[:item] = params[:item]
+        @item = session[:item]
+
+        #binding.pry
+
+        erb :checkout
+    end
 end
